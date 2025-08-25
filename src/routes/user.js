17c5b47553163bef44692b121fe532b31a2af560
Nizamuddin Shaikh <nizamuddin.shaikhh@gmail.com>
2025-08-25 23:57:38 +0530

    added feed

--- src/routes/user.js
+++ src/routes/user.js
@@ -5,12 +5,37 @@ const userRouter = express.Router();
 const userAuth = require('../middlewares/auth');
 const connection = require('../models/connection');
 
+userRouter.get('/users/requests/received', userAuth, async (req, res) => {
+    const user = req.user;
+    const connections = await connection.find(
+            { recipientId: user._id, status: 'pending' }
+    ).populate('requesterId', ['firstName', 'lastName', 'skills', 'shortBio']);
+    res.status(200).json(connections);
+});
+
+userRouter.get('/users/connections', userAuth, async (req, res) => {
+    const user = req.user;
+    const connections = await connection.find(
+            { $or: [ { requesterId: user._id}, { recipientId: user._id} ], status: 'accepted' }
+    ).populate('requesterId recipientId', ['firstName', 'lastName', 'skills', 'shortBio']);
+    res.status(200).json(connections);
+});
+
 userRouter.get('/users/feed', userAuth, async (req, res) => {
     const user = req.user;
     const connections = await connection.find(
-            { requesterId: user._id}, {status: 'accepted'}
-    ).populate('recipientId', ['firstName', 'lastName', 'skills', 'shortBio']);
-    res.status(200).json(connections);
+            { $or: [ { requesterId: user._id}, { recipientId: user._id} ], status: 'accepted' }
+    );
+
+    const connectedUserIds = connections.map(conn => 
+        (conn.requesterId.toString() === user._id.toString()) ? conn.recipientId : conn.requesterId
+    );
+
+    const feedUsers = await User.find({ 
+        _id: { $nin: [...connectedUserIds, user._id] } 
+    }).select('firstName lastName skills shortBio');
+
+    res.status(200).json(feedUsers);
 });
 
 module.exports = userRouter;
